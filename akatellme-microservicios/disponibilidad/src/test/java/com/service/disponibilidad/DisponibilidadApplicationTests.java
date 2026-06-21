a8708ade44c5aa406d5a86fb208f3939d684aea5
Ian Muñoz <iaa.munozc@duocuc.cl>
2026-06-20 20:51:45 -0400

    Implementacion del microservicio disponibilidad y configuracion de swagger, actualizacion de gateway

---
+++ akatellme-microservicios/disponibilidad/src/test/java/com/service/disponibilidad/DisponibilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.disponibilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisponibilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
